844c1d5400965c638e52a68ec937b40ec5c4b594
wheezy-code <niranjrajesh7007@gmail.com>
2021-02-01 20:15:56 +0530

    bug fix login error

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -4,15 +4,18 @@ from store.models import Customer
 from django.contrib.auth import get_user_model
 
 
-User=get_user_model()
+User = get_user_model()
+
 
 class UserAdminCreationForm(forms.ModelForm):
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'placeholder':'Password'}))
-    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput(attrs={'placeholder':'ConfirmPassword'}))
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(
+        attrs={'placeholder': 'Password'}))
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput(
+        attrs={'placeholder': 'ConfirmPassword'}))
 
     class Meta:
         model = User
-        fields = ('email','aadhaar')
+        fields = ('email', 'aadhaar')
 
     def clean_password2(self):
         password1 = self.cleaned_data.get("password1")
@@ -31,30 +34,37 @@ class UserAdminCreationForm(forms.ModelForm):
 
 
 class UserAdminChangeForm(forms.ModelForm):
-   
+
     password = ReadOnlyPasswordHashField()
 
     class Meta:
         model = User
-        fields = ('email', 'aadhaar','password', 'active', 'admin')
+        fields = ('email', 'aadhaar', 'password', 'active', 'admin')
 
     def clean_password(self):
         return self.initial["password"]
 
 
 class LoginForm(forms.Form):
-    email=forms.EmailField(label='Email',widget=forms.EmailInput(attrs={'placeholder':'Email'}))
-    password=forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Password'}))
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(
+        attrs={'placeholder': 'Email'}))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={'placeholder': 'Password'}))
+
 
 class SignUpForm(forms.ModelForm):
-    email=forms.EmailField(label='Email',widget=forms.EmailInput(attrs={'placeholder':'Email'}))
-    aadhaar=forms.CharField(label='aadhaar',widget=forms.TextInput(attrs={'placeholder':'Aadhaar'}),max_length=12)
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'placeholder':'Password'}))
-    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput(attrs={'placeholder':'ConfirmPassword'}))
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(
+        attrs={'placeholder': 'Email'}))
+    aadhaar = forms.CharField(label='aadhaar', widget=forms.TextInput(
+        attrs={'placeholder': 'Aadhaar'}), max_length=12)
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(
+        attrs={'placeholder': 'Password'}))
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput(
+        attrs={'placeholder': 'ConfirmPassword'}))
 
     class Meta:
         model = User
-        fields = ('email','aadhaar')
+        fields = ('email', 'aadhaar')
 
     def clean_password2(self):
         password1 = self.cleaned_data.get("password1")
@@ -64,19 +74,16 @@ class SignUpForm(forms.ModelForm):
         return password2
 
     def clean_aadhaar(self):
-        data=self.cleaned_data.get("aadhaar")
-        checkingData=Customer.objects.filter(aadhaar__contains=data)
+        data = self.cleaned_data.get("aadhaar")
+        checkingData = Customer.objects.filter(aadhaar__contains=data)
         if not checkingData:
             raise forms.ValidationError("Aadhaar Invalid")
         return data
 
-  
-
-        
     def save(self, commit=True):
         user = super(SignUpForm, self).save(commit=False)
         user.set_password(self.cleaned_data["password1"])
         if commit:
-            user.active=False;
+            user.active = False
             user.save()
         return user
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,14 +1,14 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 
 from django.contrib.auth.hashers import check_password
 from django.http import HttpResponse, HttpResponseRedirect
-from .forms import SignUpForm,LoginForm
-from  django.contrib import messages
+from .forms import SignUpForm, LoginForm
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from store.models import Customer
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
-from django.contrib  import sessions
+from django.contrib import sessions
 from django.views.decorators.csrf import csrf_protect
 from .decorators import unauthenticated_user
 from django.http import JsonResponse
@@ -18,89 +18,93 @@ import http
 import ast
 import json
 import os
-conn=http.client.HTTPConnection("2factor.in")
+conn = http.client.HTTPConnection("2factor.in")
 
-User=get_user_model()
+User = get_user_model()
 
 
 # Create your views here.
 @csrf_protect
 @unauthenticated_user
 def sign(request):
-    usererror=''
+    usererror = ''
     User.objects.filter(active=False).delete()
-    form2=LoginForm(request.POST or None)
+    form2 = LoginForm(request.POST or None)
     if request.POST.get('Login'):
         if form2.is_valid():
-            email=form2.cleaned_data.get("email")
-            password=form2.cleaned_data.get("password") 
-            user= authenticate(request,username=email,password=password)
+            email = form2.cleaned_data.get("email")
+            password = form2.cleaned_data.get("password")
+            user = authenticate(request, username=email, password=password)
             if user is not None:
-                login(request,user)
+                login(request, user)
                 if user.is_delivery_staff:
                     return redirect('delivery')
                 else:
-                     return redirect('products')
-            else:      
-                passUser=User.objects.get(email=email)
-                answer=passUser.check_password(password)
+                    return redirect('products')
+            else:
+                try:
+                    passUser = User.objects.get(email=email)
+                except User.DoesNotExist:
+                    usererror = "Enter a valid Email"
+                    passUser = None
+                answer = passUser.check_password(password)
                 if(not answer):
-                    usererror="Invalid Password"
-     
-    context={'form2':form2,'usererror':usererror}            
-    return render(request,'accounts/sign.html',context)
+                    usererror = "Invalid Password"
 
+    context = {'form2': form2, 'usererror': usererror}
+    return render(request, 'accounts/sign.html', context)
 
 
 @csrf_protect
 def createUser(request):
-    errormsg=''
-    if request.method =="POST":
-        data=json.loads(request.body)
-        email=data["userData"]["email"]
-        aadhaar=data["userData"]["aadhaar"]
-        password=data["userData"]["password"]
-        confirmpassword=data["userData"]["confirmpassword"]
-        newdata={
-            'email':email,
-            'aadhaar':aadhaar,
-            'password1':password,
-            'password2':confirmpassword,
-            
+    errormsg = ''
+    if request.method == "POST":
+        data = json.loads(request.body)
+        email = data["userData"]["email"]
+        aadhaar = data["userData"]["aadhaar"]
+        password = data["userData"]["password"]
+        confirmpassword = data["userData"]["confirmpassword"]
+        newdata = {
+            'email': email,
+            'aadhaar': aadhaar,
+            'password1': password,
+            'password2': confirmpassword,
+
         }
-        form=SignUpForm(newdata)
+        form = SignUpForm(newdata)
         if form.is_valid():
-            aadhaar=form.cleaned_data['aadhaar']
+            aadhaar = form.cleaned_data['aadhaar']
             try:
-                checkingAadhaar=Customer.objects.get(aadhaar=aadhaar)
-                
+                checkingAadhaar = Customer.objects.get(aadhaar=aadhaar)
+
             except Customer.DoesNotExist:
-                checkingAadhaar=False 
+                checkingAadhaar = False
             if checkingAadhaar:
-                key= send_otp()
-                old=checkingAadhaar.phoneOtp
+                key = send_otp()
+                old = checkingAadhaar.phoneOtp
                 if(old > 10):
-                    status="Sending Otp error"     
-                checkingAadhaar.phoneOtp=old+1
+                    status = "Sending Otp error"
+                checkingAadhaar.phoneOtp = old+1
                 checkingAadhaar.save()
-                phone=checkingAadhaar.phoneNumber
-                conn.request("GET","https://2factor.in/API/V1/a8254471-352a-11eb-83d4-0200cd936042/SMS/"+str(phone)+"/"+str(key)+"/supplyco")
-                res=conn.getresponse()
-                data=res.read()
-                data=data.decode("utf-8")
-                data=ast.literal_eval(data)
+                phone = checkingAadhaar.phoneNumber
+                conn.request("GET", "https://2factor.in/API/V1/a8254471-352a-11eb-83d4-0200cd936042/SMS/" +
+                             str(phone)+"/"+str(key)+"/supplyco")
+                res = conn.getresponse()
+                data = res.read()
+                data = data.decode("utf-8")
+                data = ast.literal_eval(data)
                 form.save()
-                request.session['key']=key
-                request.session['aadhaar']=aadhaar
-                success=True
-                
+                request.session['key'] = key
+                request.session['aadhaar'] = aadhaar
+                success = True
+
         else:
-            errormsg=form.errors.as_json()
-            errormsg=json.loads(errormsg)
-            success=False
-          
-    return JsonResponse(json.dumps({'errormsg':errormsg,'success':success}),safe=False)    
-    
+            errormsg = form.errors.as_json()
+            errormsg = json.loads(errormsg)
+            success = False
+
+    return JsonResponse(json.dumps({'errormsg': errormsg, 'success': success}), safe=False)
+
 
 # @receiver(post_save)
 # def  checkUser(sender,**kwargs):
@@ -109,28 +113,28 @@ def createUser(request):
 #                data=kwargs["instance"].aadhaar
 #                checkingData=Customer.objects.filter(aadhaar__contains=data)
 #                if checkingData:
-#                    checkingData.update(user_id=kwargs["instance"].id)   
+#                    checkingData.update(user_id=kwargs["instance"].id)
 
 def send_otp():
-    key=random.randint(1000,10000)
+    key = random.randint(1000, 10000)
     return key
+
 
 @csrf_protect
 def otp(request):
-    if request.method=='POST':
-        key=request.session['key']
-        aadhaar=request.session['aadhaar']
-        keyFromFront=request.body.decode('utf-8')
+    if request.method == 'POST':
+        key = request.session['key']
+        aadhaar = request.session['aadhaar']
+        keyFromFront = request.body.decode('utf-8')
         if keyFromFront == str(key):
-            user=User.objects.get(aadhaar=aadhaar)
-            customer=Customer.objects.filter(aadhaar=aadhaar)
+            user = User.objects.get(aadhaar=aadhaar)
+            customer = Customer.objects.filter(aadhaar=aadhaar)
             customer.update(user_id=user.id)
-            user.active=True
+            user.active = True
             user.save()
-            success=True
+            success = True
         else:
-            success=False    
-        return JsonResponse(json.dumps(success),safe=False)     
-    context={}    
-    return render(request,'accounts/otp.html',context)
-    
+            success = False
+        return JsonResponse(json.dumps(success), safe=False)
+    context = {}
+    return render(request, 'accounts/otp.html', context)
